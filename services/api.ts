aba2ed92989c98f8d0aa1aa8efca77eff8e935e8
Emre Uslu <emreuslu256@gmail.com>
2026-03-29 16:33:17 +0300

    feat: add API service and custom hook for data fetching

---
+++ services/api.ts
@@ -0,0 +1,42 @@
+/* const url = "https://api.themoviedb.org/3/discover/movie";
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MzM0NTViZjE2MGZlMTY3OWQyMDg0YTI0ZWU1YTE1NSIsIm5iZiI6MTc3NDc4OTQ1NS42MjcsInN1YiI6IjY5YzkyMzRmODBjYTI3Njk2NmU0NDYyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.k3zCY_0gs7vkQE7_1f1D1B0Yg093e_YsSrSiKo7g3mY",
+  },
+};
+
+fetch(url, options)
+  .then((res) => res.json())
+  .then((json) => console.log(json))
+  .catch((err) => console.error(err));
+*/
+
+export const TMBD_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API,
+  headers: {
+    accept: "application/json",
+    authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API}`,
+  },
+};
+
+export const fetchPopularMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `${TMBD_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMBD_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: TMBD_CONFIG.headers,
+  });
+
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error("Failed to fetch movies", response.statusText);
+  }
+
+  const data = await response.json();
+  return data.results;
+};
